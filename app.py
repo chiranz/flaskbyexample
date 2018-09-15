c15d92df63ac10681079db9ec76d09442cd6d73e
Chiranz <chiranpoudyal@gmail.com>
2018-09-15 11:56:57 +0530

    V 0.1

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ app = Flask(__name__)
 
 app.config.from_object(os.environ['APP_SETTINGS'])
 
+
+
 @app.route('/')
 def hello():
 	return "Hello World!"
@@ -11,6 +13,10 @@ def hello():
 @app.route('/<name>')
 def hello_name(name):
 	return "Hello {}!".format(name)
+
+@app.route('/register')
+def register():
+	return "<h1> Work under Process</h2>"
 
 print(os.environ['APP_SETTINGS'])
 if __name__ == '__main__':
